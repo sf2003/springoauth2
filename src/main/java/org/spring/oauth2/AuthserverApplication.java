c1f7fb3da6feb92d8a0eca2f3666085b1062ed13
fu shen <scanf1983@gmail.com>
2019-01-09 16:48:22 +0800

    oath2认证

---
+++ src/main/java/org/spring/oauth2/AuthserverApplication.java
@@ -0,0 +1,189 @@
+package org.spring.oauth2;
+
+import java.io.IOException;
+import java.security.Principal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.ManagementServerProperties;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.endpoint.AuthorizationEndpoint;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@Controller
+public class AuthserverApplication extends WebMvcConfigurerAdapter {
+
+	private Logger logger = LoggerFactory.getLogger(AuthserverApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthserverApplication.class, args);
+	}
+
+
+	@Autowired
+	private AuthorizationEndpoint authorizationEndpoint;
+
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("login2");
+		registry.addViewController("/oauth/confirm_access").setViewName("authorize");
+	}
+
+	@Configuration
+	@EnableWebSecurity
+	@Order(ManagementServerProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class LoginConfig extends WebSecurityConfigurerAdapter {
+		
+//		@Autowired
+//		private AuthenticationManager authenticationManager;
+
+		@Autowired
+		private UserDetailsService authUserDetailService;
+		
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.formLogin().loginPage("/login").permitAll().and().authorizeRequests()
+					.anyRequest().authenticated();
+		}
+		
+		@Override
+		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+			auth.userDetailsService(authUserDetailService);
+		}
+	}
+	
+	@Configuration
+	@EnableResourceServer
+	@RestController
+	protected static class ResourceServerConfig  extends ResourceServerConfigurerAdapter {
+		
+	    @Override
+	    public void configure(HttpSecurity http) throws Exception {
+			http.antMatcher("/oauth/user/**")
+					.authorizeRequests().anyRequest().authenticated();
+		}
+	    
+	    @Override
+		public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+			super.configure(resources);
+			resources.tokenServices(tokenServices());
+		}
+	    
+		@RequestMapping("/oauth/user")
+		public Principal user(Principal user) {
+			return user;
+		}
+
+		@Bean
+		public TokenStore tokenStore() {
+			return new JwtTokenStore(accessTokenConverter());
+		}
+
+		@Bean
+		public JwtAccessTokenConverter accessTokenConverter() {
+			JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+			converter.setSigningKey("123");
+			return converter;
+		}
+
+		@Bean
+		@Primary
+		public DefaultTokenServices tokenServices() {
+			DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+			defaultTokenServices.setTokenStore(tokenStore());
+			return defaultTokenServices;
+		}
+	}
+	
+	
+	@Configuration
+	@EnableAuthorizationServer
+	protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+		@Autowired
+		private AuthenticationManager authenticationManager;
+
+		@Override
+		public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+			endpoints.authenticationManager(this.authenticationManager);
+			endpoints.accessTokenConverter(accessTokenConverter());
+			endpoints.tokenStore(tokenStore());
+		}
+
+
+		@Override
+		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+			clients.inMemory()
+					.withClient("clientiop")
+					.secret("clientsecretiop")
+					.authorizedGrantTypes("authorization_code", "refresh_token",
+							"password","client_credentials").scopes("trust").autoApprove(true);
+		}
+
+		@Bean
+		public TokenStore tokenStore() {
+			return new JwtTokenStore(accessTokenConverter());
+		}
+
+		@Bean
+		public JwtAccessTokenConverter accessTokenConverter() {
+			JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+			converter.setSigningKey("123");
+			return converter;
+		}
+
+		@Bean
+		@Primary
+		public DefaultTokenServices tokenServices() {
+			DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+			defaultTokenServices.setTokenStore(tokenStore());
+			defaultTokenServices.setSupportRefreshToken(true);
+			return defaultTokenServices;
+		}
+	}
+
+}
